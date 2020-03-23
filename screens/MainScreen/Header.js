e8cc92a64d356fa7b1d073ac06917b532b03981d
Rishav78 <rishavgarg789@gmail.com>
2020-03-23 17:04:29 +0530

    change theme button added

--- screens/MainScreen/Header.js
+++ screens/MainScreen/Header.js
@@ -1,35 +1,73 @@
-import React, { useState } from 'react';
-import {View} from 'react-native'
+import React, { useState, useContext } from 'react';
+import {
+    StyleSheet,
+    Text
+} from 'react-native'
 import {
     Appbar,
-    Menu,
-    Provider
+    Menu
 } from 'react-native-paper';
+import { ThemeContext } from '../../src/contexts/ThemeContext';
 
 const ScreenHeader = props => {
+    const { theme, setTheme } = useContext(ThemeContext);
     const [menuVisibility, setMenuVisibility] = useState(false);
+
+    const changeTheme = (theme) => {
+        setTheme(theme);
+        setMenuVisibility(false);
+    }
+
     return (
-        <Appbar.Header>
-            <Appbar.BackAction
-            />
+        <Appbar.Header
+            style={ theme === 'dark' ? {...stylesCommon.header, ...stylesDarkTheme.header} : stylesCommon.header}
+        >
+
             <Appbar.Content
-            title="Title"
-            subtitle="Subtitle"
-            />
-            <Appbar.Action icon="magnify" />
+                title="Todo"/>
             <Menu
                 visible={menuVisibility}
                 onDismiss={() => setMenuVisibility(false)}
                 anchor={
-                    <Appbar.Action icon="dots-vertical" onPress={() => setMenuVisibility(true)} />
-                }
-            >
-                <Menu.Item onPress={() => {}} title="Item 1" />
-                <Menu.Item onPress={() => {}} title="Item 2" />
-                <Menu.Item onPress={() => {}} title="Item 3" />
+                    <Appbar.Action 
+                        color="#FFF"
+                        icon="dots-vertical" onPress={() => setMenuVisibility(true)} />
+                }>
+                { theme === 'dark' ?
+                 <Menu.Item 
+                    style={theme === 'dark' ? stylesDarkTheme.items : {}} 
+                    onPress={() => changeTheme('light')} 
+                    title={<Text style={ theme === 'dark' ?{ color: 'white'} : {}}>Light Theme</Text>}/>
+                :
+                 <Menu.Item  
+                    style={theme === 'dark' ? stylesDarkTheme.items : {}}
+                    onPress={() => changeTheme('dark')} 
+                    title={<Text style={ theme === 'dark' ?{ color: 'white'} : {}}>Dark Theme</Text>} />}
             </Menu>
         </Appbar.Header>
     );
 }
+
+const stylesCommon = StyleSheet.create({
+    header: {
+        color: 'white'
+    }
+});
+
+
+const stylesDarkTheme = StyleSheet.create({
+    header: {
+        backgroundColor: '#262626'
+    },
+    menu: {
+        backgroundColor: '#262626',
+        padding: 0,
+        margin: 0
+    },
+    items: {
+        backgroundColor: '#262626',
+        color: '#FFF'
+    }
+})
 
 export default ScreenHeader;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = props => {
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('light');
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
             {props.children}
